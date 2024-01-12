d803ecd7f170ec4bcc7d74040165edb84df9987d
Rpyung23 <guaman1579@gmail.com>
2024-01-12 15:57:39 -0500

    update mvc

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 require('./config/port')
+const cors = require('cors')
+const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
+
+
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+
 
 app.listen(process.env.PORT,()=>{
     console.log(`SERVER API ${process.env.PORT}`)
