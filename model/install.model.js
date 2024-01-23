fd300189606d89794a15ddcf221fe60462ed0b28
Rpyung23 <guaman1579@gmail.com>
2024-01-23 13:03:26 -0500

    update

--- model/install.model.js
+++ model/install.model.js
@@ -5,7 +5,7 @@ class InstallModel
     {
         try {
             var conn = await connDB().promise()
-            var sql = "select I.id,I.name,I.dir,I.ciudad,I.tarea,I.ref,I.notas,I.id_tec," +
+            var sql = "select I.id,I.name,I.cel,I.dir,I.ciudad,I.tarea,I.ref,I.notas,I.id_tec," +
                 "U.nombre as nombre_tecnico from instalaciones as I inner join users as U " +
                 "on U.id = I.id_tec where I.estado = 0 order by I.fecha desc"
             //console.log(sql)
