7dd53ab69bbb2e0708ad730e15adaa798d83f960
Rpyung23 <guaman1579@gmail.com>
2024-02-25 16:24:20 -0500

    updaet

--- model/transfers.model.js
+++ model/transfers.model.js
@@ -85,6 +85,7 @@ class TransfersModel
         try {
             var conn = await connDB().promise()
             var sql = "update traspasos set estado = "+estado+" where id = "+id
+            console.log(sql)
             await conn.query(sql)
             await conn.end()
             return {code :200,msm:"OK"}
